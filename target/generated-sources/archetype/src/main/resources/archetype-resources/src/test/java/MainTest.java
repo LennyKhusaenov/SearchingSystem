5ee9cfd75d5408bbb0e45bb56d7d7349d2531a57
LennyKhusaenov <leniz.khusaenov@mail.ru>
2017-04-14 14:04:55 +0300

    Create mvn archetype for creating simple DB indexator in target/generated-sources

---
+++ target/generated-sources/archetype/src/main/resources/archetype-resources/src/test/java/MainTest.java
@@ -0,0 +1,41 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Main.
+ */
+public class MainTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MainTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( MainTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
---
+++ target/generated-sources/archetype/target/classes/archetype-resources/src/main/java/Tokenizer.java
@@ -0,0 +1,103 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tokenizer {
+
+    private String word;
+    private int lineNumber;
+    private int startIndex;
+    private int endIndex;
+
+    public String getDocName() {
+        return docName;
+    }
+
+    public String docName;
+
+    public Tokenizer(String word, int lineNumber, int startIndex, int endIndex,
+                     String docName) {
+        this.word = word;
+        this.lineNumber = lineNumber;
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+        this.docName = docName;
+    }
+
+    public static boolean isWord(char ch) {
+        return Character.isLetter(ch);
+    }
+
+
+    public static void tokenize(String inputStr, int lineNum, String filename, List<Tokenizer> str) {
+
+        char[] charArr = inputStr.toCharArray();
+        boolean flag = false;
+        int stInTemp = 0;
+        for (int i = 0; i < charArr.length; i++) {
+            String temp = "";
+            while (i < charArr.length && isWord(charArr[i])) {
+                temp += charArr[i];
+
+                if (flag == false) stInTemp = i;
+                flag = true;
+                i++;
+            }
+            if (temp.compareTo("") > 0)
+                str.add(new Tokenizer(temp, lineNum, stInTemp, stInTemp + temp.length(), filename));
+            flag = false;
+
+
+        }
+    }
+
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public void setLineNumber(int lineNumber) {
+        this.lineNumber = lineNumber;
+    }
+
+    public int getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(int startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    public int getEndIndex() {
+        return endIndex;
+    }
+
+    public void setEndIndex(int endIndex) {
+        this.endIndex = endIndex;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "(word='" + word + '${symbol_escape}'' +
+                        ", lineNumber=" + lineNumber +
+                        ", startIndex=" + startIndex +
+                        ", endIndex=" + endIndex +
+//                        ", filename=" + docName +
+                        ")    ";
+    }
+
+
+}
